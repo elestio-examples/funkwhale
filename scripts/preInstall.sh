895de0d49f0ee90573d1a40b83aae1cacc3491da
AmsellemJoseph <amsellem.joseph@gmail.com>
2022-12-18 09:00:01 +0200

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -3,5 +3,13 @@ set -o allexport; source .env; set +o allexport;
 
 mkdir -p ./data
 mkdir -p ./dir
+mkdir -p ./media
+mkdir -p ./music
+mkdir -p ./static
+mkdir -p ./frontend
 chown -R 1000:1000 ./data
 chown -R 1000:1000 ./dir
+chown -R 1000:1000 ./media
+chown -R 1000:1000 ./music
+chown -R 1000:1000 ./static
+chown -R 1000:1000 ./frontend
