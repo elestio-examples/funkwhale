078c3fa4ec3116a137ae83d713c08fe8b87de140
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-08-31 16:20:25 +0300

    fix

--- scripts/preInstall.sh
+++ scripts/preInstall.sh
@@ -1,15 +1,14 @@
 set env vars
 set -o allexport; source .env; set +o allexport;
 
-mkdir -p ./data
-mkdir -p ./dir
-mkdir -p ./media
-mkdir -p ./music
-mkdir -p ./static
-mkdir -p ./frontend
-chown -R 600:600 ./data
-chown -R 600:600 ./dir
-chown -R 600:600 ./media
-chown -R 600:600 ./music
-chown -R 600:600 ./static
-chown -R 600:600 ./frontend
+mkdir -p ./funkwhale
+
+chown -R 600:600 ./funkwhale
+
+
+DJANGO_SECRET_KEY=$(openssl rand -base64 45)
+
+cat << EOT >> ./.env
+
+DJANGO_SECRET_KEY=${DJANGO_SECRET_KEY}
+EOT
